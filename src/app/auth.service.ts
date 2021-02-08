ab7e574714442a6a9f8c8620d9d0da9a82ed7100
Luis Huertas <hu3rtas@gmail.com>
2021-02-08 12:25:44 -0500

    TEST

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -20,6 +20,8 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  x = 'TEST';
+
 
   cambiarContra(passwordForm: { passwordOld: string, password: string }, id: string): Observable<{ changed: boolean }> {
     return this.http.post<{ changed: boolean }>(this.baseUrl + 'auth/cambiarContrasena', { ...passwordForm, id })
