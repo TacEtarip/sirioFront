5b1e5c4ab0ae7f160cebe2567ddd399c4bcf72cf
Luis Huertas <hu3rtas@gmail.com>
2021-03-07 16:16:23 -0500

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { NotFoundPageComponent } from './not-found-page/not-found-page.component
     BrowserAnimationsModule,
     MatFormFieldModule,
     HttpClientModule,
+    TransferHttpCacheModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [{
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -4,10 +4,13 @@ import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
+import { ServerTransferStateModule } from '@angular/platform-server';
+
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    ServerTransferStateModule
   ],
   bootstrap: [AppComponent],
 })
